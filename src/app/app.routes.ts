758e2138e519d7ab72ff10d8f3f9f373a4243935
Yash Bansal <dilboleoberoy@gmail.com>
2024-09-23 23:02:46 +0530

    card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { CheckcompComponent } from './checkcomp/checkcomp.component';
 import { Checkcomp2Component } from './checkcomp2/checkcomp2.component';
 
 export const routes: Routes = [
-    {path:'', component: AppComponent},
+    {path:'', component: CheckcompComponent},
     {path:'about',  component: CheckcompComponent},
     {path:'about2',  component: Checkcomp2Component},
 ];
--- src/app/checkcomp/checkcomp.component.ts
+++ src/app/checkcomp/checkcomp.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-checkcomp',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './checkcomp.component.html',
   styleUrl: './checkcomp.component.css'
 })
